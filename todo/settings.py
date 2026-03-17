0f8d54c85ab79b88cebc633cf9cba23be61addf7
kaushal-Prakash <kingkaushal1289@gmail.com>
2026-03-17 15:05:49 +0530

    add initial migration for Student and Course models, update settings and urls to include stud_db

--- todo/settings.py
+++ todo/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # "main",
-    "template_inheritance",
+    # "template_inheritance",
+    "stud_db",
 ]
 
 MIDDLEWARE = [
--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     # path("", include("main.urls")),   # maps main app url from this urls file
-    path("", include("template_inheritance.urls")),   # maps template_inheritance app url from this urls file
+    # path("", include("template_inheritance.urls")),   # maps template_inheritance app url from this urls file
+    path("", include("stud_db.urls")),   # maps stud_db app url from this urls file
 ]
