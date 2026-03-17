0d38d619e867580f609c69580b136e35f919990f
kaushal-Prakash <kingkaushal1289@gmail.com>
2026-03-17 14:54:03 +0530

    added student and course model

--- stud_db/models.py
+++ stud_db/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    coursecode = models.CharField(max_length=20, unique=True)
+    coursename = models.CharField(max_length=100)
+    credits = models.IntegerField()
+    
+    def __str__(self):
+        return self.coursecode + " - " + self.coursename
+
+
+class Student(models.Model):
+    usn = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=100)
+    sem = models.IntegerField()
+    courses = models.ManyToManyField(Course, related_name='students')
+
+    def __str__(self):
+        return self.usn + " - " + self.name
