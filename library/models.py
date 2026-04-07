7bc32659fff427d7ee96eae44e1b20a472f645b2
kaushal-Prakash <kingkaushal1289@gmail.com>
2026-04-07 15:25:07 +0530

    initialised project and added models: Book, Author and publisher

---
+++ library/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    publisher = models.ForeignKey('Publisher', on_delete=models.CASCADE)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
+
+    def __str__(self):
+        return self.title
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
