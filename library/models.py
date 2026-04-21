0e19526e7898aa245f6ca5a4bd552e689dc79669
kaushal-Prakash <kingkaushal1289@gmail.com>
2026-04-21 14:44:20 +0530

    added new student_form app

--- library/models.py
+++ library/models.py
@@ -6,6 +6,7 @@ class Book(models.Model):
     publisher = models.ForeignKey('Publisher', on_delete=models.CASCADE)
     published_date = models.DateField()
     isbn = models.CharField(max_length=13, unique=True)
+    cover_image = models.ImageField(upload_to='book_covers/', null=True, blank=True)
 
     def __str__(self):
         return self.title
